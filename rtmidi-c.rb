2c3c84393bec3f270d4aae2266adc4f9820ac759
Ben Swift <benjamin.j.swift@gmail.com>
2013-11-05 16:09:11 +1100

    clean up rtmidi-c formula

--- rtmidi-c.rb
+++ rtmidi-c.rb
@@ -3,12 +3,11 @@ require 'formula'
 class RtmidiC < Formula
   homepage 'https://github.com/benswift/rtmidi-c-api'
   url 'https://github.com/benswift/rtmidi-c-api/archive/master.zip'
-  sha1 '5e514d43131125a5b186a8e4725656dca5de309e'
+  sha1 '9225c0f05b0c1070cc31fe81518634e7dce0a4ab'
   version '0.1'
   
   def install
-    # this is pretty filthy.  Sorry everyone.
-    system "g++ -dynamiclib -Wall -D__MACOSX_CORE__ RtMidi.cpp RtMidi-C-Api.cpp -framework CoreMIDI -framework CoreAudio -framework CoreFoundation -o librtmidic.dylib"
+    system "make", "-f", "Makefile-osx"
     include.install('RtError.h', 'RtMidi-C-Api.h', 'RtMidi.h')
     lib.install('librtmidic.dylib')
   end
