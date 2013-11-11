73c20282661c9930a5cf157d2be2943f98d232fe
Ben Swift <benjamin.j.swift@gmail.com>
2013-11-12 09:18:19 +1100

    update kissfft formula, now passes audit

--- kissfft.rb
+++ kissfft.rb
@@ -7,9 +7,12 @@ class Kissfft < Formula
   sha1 '292f06a7a6fcb658e01d97dad71cfd679741b3fb'
 
   def install
-    system "gcc kiss_fft.c tools/kiss_fftr.c -dynamiclib -I. -I/usr/include/malloc -o kiss_fft.1.3.0.dylib -current_version 1.3.0"
-    system "ln -s kiss_fft.1.3.0.dylib kiss_fft.dylib"
+    system ENV.cc, "kiss_fft.c", "tools/kiss_fftr.c",
+                   "-I.", "-I/usr/include/malloc",
+                   "-dynamiclib", "-current_version", "#{version}",
+                   "-o", "kiss_fft.#{version}.dylib"
+    system "ln", "-s", "kiss_fft.#{version}.dylib", "kiss_fft.dylib"
     include.install "kiss_fft.h"
-    lib.install "kiss_fft.1.3.0.dylib", "kiss_fft.dylib"
+    lib.install "kiss_fft.#{version}.dylib", "kiss_fft.dylib"
   end
 end
