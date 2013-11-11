4a5ea16d06468950e6d214ed87445bce44016704
Ben Swift <benjamin.j.swift@gmail.com>
2013-11-12 09:21:25 +1100

    update rtmidi formula (just formatting)

--- rtmidi-c.rb
+++ rtmidi-c.rb
@@ -5,10 +5,10 @@ class RtmidiC < Formula
   url 'https://github.com/benswift/rtmidi-c-api/archive/master.zip'
   sha1 '9225c0f05b0c1070cc31fe81518634e7dce0a4ab'
   version '0.1'
-  
+
   def install
     system "make", "-f", "Makefile-osx"
-    include.install('RtError.h', 'RtMidi-C-Api.h', 'RtMidi.h')
-    lib.install('librtmidic.dylib')
+    include.install "RtError.h", "RtMidi-C-Api.h", "RtMidi.h"
+    lib.install "librtmidic.dylib"
   end
 end
