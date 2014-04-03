64b14b070c284cd8935ab149e07b47a2df618fc8
Ben Swift <benjamin.j.swift@gmail.com>
2014-04-03 14:17:19 +1100

    update extempore-llvm patch to fix out-of-date C++ usage
    behaviour was ok in older OSX toolchains, but from 10.9.2 onwards it's
    no longer ok.
    
    closes GH-2

--- extempore-llvm.rb
+++ extempore-llvm.rb
@@ -55,11 +55,73 @@ class ExtemporeLlvm < Formula
 end
 
 __END__
-diff --git a/lib/AsmParser/LLParser.cpp b/lib/AsmParser/LLParser.cpp
-index b24291f..f01b2ac 100644
+diff -ruN a/include/llvm/Support/CFG.h b/include/llvm/Support/CFG.h
+--- a/include/llvm/Support/CFG.h
++++ b/include/llvm/Support/CFG.h
+@@ -27,8 +27,9 @@
+ 
+ template <class Ptr, class USE_iterator> // Predecessor Iterator
+ class PredIterator : public std::iterator<std::forward_iterator_tag,
+-                                          Ptr, ptrdiff_t> {
+-  typedef std::iterator<std::forward_iterator_tag, Ptr, ptrdiff_t> super;
++                                          Ptr, ptrdiff_t, Ptr*, Ptr*> {
++  typedef std::iterator<std::forward_iterator_tag, Ptr, ptrdiff_t, Ptr*,
++                                                                    Ptr*> super;
+   typedef PredIterator<Ptr, USE_iterator> Self;
+   USE_iterator It;
+ 
+@@ -40,6 +41,7 @@
+ 
+ public:
+   typedef typename super::pointer pointer;
++  typedef typename super::reference reference;
+ 
+   PredIterator() {}
+   explicit inline PredIterator(Ptr *bb) : It(bb->use_begin()) {
+@@ -50,7 +52,7 @@
+   inline bool operator==(const Self& x) const { return It == x.It; }
+   inline bool operator!=(const Self& x) const { return !operator==(x); }
+ 
+-  inline pointer operator*() const {
++  inline reference operator*() const {
+     assert(!It.atEnd() && "pred_iterator out of range!");
+     return cast<TerminatorInst>(*It)->getParent();
+   }
+@@ -100,10 +102,11 @@
+ 
+ template <class Term_, class BB_>           // Successor Iterator
+ class SuccIterator : public std::iterator<std::bidirectional_iterator_tag,
+-                                          BB_, ptrdiff_t> {
++                                          BB_, ptrdiff_t, BB_*, BB_*> {
+   const Term_ Term;
+   unsigned idx;
+-  typedef std::iterator<std::bidirectional_iterator_tag, BB_, ptrdiff_t> super;
++  typedef std::iterator<std::bidirectional_iterator_tag, BB_, ptrdiff_t, BB_*,
++                                                                    BB_*> super;
+   typedef SuccIterator<Term_, BB_> Self;
+ 
+   inline bool index_is_valid(int idx) {
+@@ -112,6 +115,7 @@
+ 
+ public:
+   typedef typename super::pointer pointer;
++  typedef typename super::reference reference;
+   // TODO: This can be random access iterator, only operator[] missing.
+ 
+   explicit inline SuccIterator(Term_ T) : Term(T), idx(0) {// begin iterator
+@@ -142,7 +146,7 @@
+   inline bool operator==(const Self& x) const { return idx == x.idx; }
+   inline bool operator!=(const Self& x) const { return !operator==(x); }
+ 
+-  inline pointer operator*() const { return Term->getSuccessor(idx); }
++  inline reference operator*() const { return Term->getSuccessor(idx); }
+   inline pointer operator->() const { return operator*(); }
+ 
+   inline Self& operator++() { ++idx; return *this; } // Preincrement
+diff -ruN a/lib/AsmParser/LLParser.cpp b/lib/AsmParser/LLParser.cpp
 --- a/lib/AsmParser/LLParser.cpp
 +++ b/lib/AsmParser/LLParser.cpp
-@@ -1349,8 +1349,14 @@ bool LLParser::ParseType(Type *&Result, bool AllowVoid) {
+@@ -1349,8 +1349,14 @@
      // If the type hasn't been defined yet, create a forward definition and
      // remember where that forward def'n was seen (in case it never is defined).
      if (Entry.first == 0) {
@@ -69,7 +131,7 @@ index b24291f..f01b2ac 100644
 +  	if (M->getTypeByName(Lex.getStrVal())) {
 +           Entry.first = M->getTypeByName(Lex.getStrVal());
 +           Entry.second = SMLoc();
-+        } else {
++        } else {	
 +      	   Entry.first = StructType::create(Context, Lex.getStrVal());
 +           Entry.second = Lex.getLoc();
 +	}
