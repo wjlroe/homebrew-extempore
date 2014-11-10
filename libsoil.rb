182239211393c82e5806ba59673c582dfc52f7d8
Ben Swift <benjamin.j.swift@gmail.com>
2014-11-09 17:36:46 -0800

    change to new (OSX-ready) libSOIL repo

--- libsoil.rb
+++ libsoil.rb
@@ -1,34 +1,15 @@
 require 'formula'
 
 class Libsoil < Formula
-  homepage 'https://github.com/smibarber/libSOIL'
-  url 'https://codeload.github.com/smibarber/libSOIL/zip/master'
+  homepage 'https://github.com/DeVaukz/SOIL'
+  url 'https://codeload.github.com/DeVaukz/SOIL/zip/master'
   version '1.07'
-  sha1 'a42baf594c02bdeeffed9fda4f0666bf3bd1725a'
+  sha1 '7114eb45fd2c8da5177e1dad663d4e09ce403842'
 
-  def patches
-    DATA
-  end
+  depends_on "cmake" => :build
 
   def install
-    system "make", "lib"
-    include.install "SOIL.h"
-    lib.install "libSOIL.a"
-    lib.install "libSOIL.dylib"
+    system "cmake", ".", *std_cmake_args
+    system "make install"
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index 6844aa6..8c1a093 100755
---- a/Makefile
-+++ b/Makefile
-@@ -30,7 +30,7 @@ lib: $(OFILES)
- 	ar -cvq $(LIBNAME).a $(OFILES)
- 	# create shared library
- 	gcc -dynamiclib -o $(DYLIBFILE) $(OFILES) $(LDFLAGS)\
--	 	-install_name $(DESTDIR)/$(LIBDIR)/$(DYLIBFILE)
-+	 	-install_name /$HOMEBREW_PREFIX/libsoil/1.07/$(DYLIBFILE)
- 
- install:
- 	$(INSTALL_DIR) $(DESTDIR)/$(INCLUDEDIR)
