b9ded5db2ace20fe636f7e5c74edde589983e92f
Ben Swift <ben@benswift.me>
2015-05-29 20:26:20 +0800

    libsoil hash changed - GH shenanigans?

--- libsoil.rb
+++ libsoil.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libsoil < Formula
   homepage 'https://github.com/DeVaukz/SOIL'
   url 'https://github.com/DeVaukz/SOIL/archive/1.07.zip'
-  sha1 '6cbb7d800a49c8df33bbe7146c6c67fb454a9777'
+  sha1 '4422d522e7f3ad6b047dab2924c6dadb7a4884d8'
 
   depends_on "cmake" => :build
 
