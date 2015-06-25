6e63bbfcd717fb9be524794fc59a55e2a89ce976
Ben Swift <ben@benswift.me>
2015-06-25 11:25:27 +1000

    update rtmidi SHA hash (GH seem to have changed their archive zipball)

--- rtmidi-c.rb
+++ rtmidi-c.rb
@@ -3,7 +3,7 @@ require 'formula'
 class RtmidiC < Formula
   homepage 'https://github.com/benswift/rtmidi'
   url 'https://github.com/benswift/rtmidi/archive/master.zip'
-  sha1 '27a702b472f13423c2da7915c8c2fbc4b6ec225e'
+  sha1 '3d34e3cbe3893121ee540fe90a8c164a9d846902'
   version '0.2'
 
   def install
