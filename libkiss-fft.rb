9a37915976e6403a6bced8099d76df21e5c977a4
Ben Swift <ben@benswift.me>
2015-09-22 10:31:26 +1000

    update sha256 for libkiss-fft

--- libkiss-fft.rb
+++ libkiss-fft.rb
@@ -1,7 +1,7 @@
 class LibkissFft < Formula
   homepage "http://kissfft.sourceforge.net/"
   url "https://github.com/benswift/kiss_fft/archive/1.3.0.zip"
-  sha256 "7a4da573d126fee5b4d9aaf9124ff02b6b85206c1ee583e21703fef5f4f383f0"
+  sha256 "c3e162c0627b1e5e77a1ac6d07e56ddc751e93735c3b5eacc8ffae41f53bf40f"
 
   depends_on "cmake" => :build
 
