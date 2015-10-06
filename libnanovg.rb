30890fcec68991bf33dad5d7ddc103e8e8931364
Ben Swift <ben@benswift.me>
2015-10-06 16:50:21 +1100

    update libnanovg formula

--- libnanovg.rb
+++ libnanovg.rb
@@ -2,18 +2,11 @@ class Libnanovg < Formula
   homepage "https://github.com/memononen/nanovg"
   head "https://github.com/benswift/nanovg.git"
 
-  depends_on 'premake' => :build
-  depends_on 'freetype' => :build
+  depends_on "freetype" => :build
+  depends_on "cmake" => :build
 
   def install
-    # set correct path to freetype
-    inreplace "premake4.lua", "includedirs { \"src\" }", "includedirs { \"src\", \"#{Formula["freetype"].include}/freetype2\" }"
-
-    system "premake4", "gmake"
-    cd "build"
-    system "make", "config=release", "nanovg"
-    cd ".."
-    include.install ["src/nanovg.h", "src/nanovg_gl.h"]
-    lib.install "build/libnanovg.dylib"
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
   end
 end
